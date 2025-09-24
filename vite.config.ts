1caa20cba471c5a728cb25869219398afaab1479
CodeByMarkh <markmwangi165@gmail.com>
2025-09-24 12:40:30 +0300

    Remove dist folder from main (handled by GitHub Actions)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/sisterhealingalliance/",
+  base: "/SisterHealingAlliance/",
   server: {
     host: "::",
     port: 8080,
